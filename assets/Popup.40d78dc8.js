2b4b1d04c33998fba381eba472e0c78411d0321f
maycorolbuche <mayco_rolbuche@hotmail.com>
2026-01-20 20:59:20 +0000

    🚀 Deploy id 21187008776

---
+++ assets/Popup.40d78dc8.js
@@ -0,0 +1,1 @@
+import{_ as o,h as t,r as n,o as r,c as s}from"./index.4dece8ad.js";import{S as c}from"./Screen.2ab920ec.js";const p={name:"PopupBiblePage",components:{Screen:c},computed:{module_id(){return t.id},module(){return this.$modules.get(this.module_id)}}};function a(m,u,_,d,i,l){const e=n("Screen");return r(),s(e)}const h=o(p,[["render",a]]);export{h as default};
